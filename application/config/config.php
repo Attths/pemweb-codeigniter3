0fafe32c30b652450d7a83dff4a270b535909794
Attths <hafishathallah0109@gmail.com>
2026-06-09 02:52:22 +0700

    konfigurasi codeigniter

--- application/config/config.php
+++ application/config/config.php
@@ -1,10 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
-$config['base_url'] .= "://" . $_SERVER['HTTP_HOST'];
-$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
-
+$config['base_url'] = 'http://localhost/masterci3/';
 $config['index_page'] = '';
 
 $config['uri_protocol']    = 'REQUEST_URI';
